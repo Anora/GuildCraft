25eb5606af9d4574589d679724b7771e83b7e823
anora <anora@dragon-services.net>
2016-05-02 14:09:48 -0300

    added missing json's
    error pop up after adding json's
     at net.minecraft.client.renderer.block.model.ModelBlock.getRootModel(ModelBlock.java:166)

--- src/main/java/anora/guildcraft/main/proxy/CommonProxy.java
+++ src/main/java/anora/guildcraft/main/proxy/CommonProxy.java
@@ -4,7 +4,6 @@ import anora.guildcraft.main.blocks.GcBlocks;
 import anora.guildcraft.main.crafting.GcRecpies;
 import anora.guildcraft.main.crafting.GcSmelting;
 import anora.guildcraft.main.items.GcItems;
-import anora.guildcraft.main.world.GcWorldGen;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
