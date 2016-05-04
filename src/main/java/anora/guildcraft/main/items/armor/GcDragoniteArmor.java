48e355aa5293c6917d2fbf3671983933db196d72
anora <anora@dragon-services.net>
2016-05-04 09:17:46 -0300

    updated todo list, added dragonite armor spacial properties and updated lang file

--- src/main/java/anora/guildcraft/main/items/armor/GcDragoniteArmor.java
+++ src/main/java/anora/guildcraft/main/items/armor/GcDragoniteArmor.java
@@ -1,7 +1,14 @@
 package anora.guildcraft.main.items.armor;
 
 import anora.guildcraft.main.GcGlobal;
+import anora.guildcraft.main.items.GcItems;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
 
 public class GcDragoniteArmor extends ItemArmor
 {
@@ -11,6 +18,45 @@ public class GcDragoniteArmor extends ItemArmor
 		super(material, renderIndex, armorType);
 		this.setUnlocalizedName(unlocalizedName);
 		this.setCreativeTab(GcGlobal.gcCreativeTab);
+	}
+	
+	@Override
+	public void onArmorTick(World world, EntityPlayer player, ItemStack itemstack)
+	{
+		if(itemstack.getItem() == GcItems.dragoniteHelmet)
+		{
+			this.effectPlayer(player, Potion.nightVision, 0);
+		}
+		if(itemstack.getItem() == GcItems.dragoniteChestplate)
+		{
+			this.effectPlayer(player, Potion.healthBoost, 1);
+		}
+		if(itemstack.getItem() == GcItems.dragoniteLeggings)
+		{
+			this.effectPlayer(player, Potion.jump, 0);
+		}
+		if(itemstack.getItem() == GcItems.dragoniteBoots)
+		{
+			this.effectPlayer(player, Potion.moveSpeed, 1);
+		}
+		if(this.isWearingFullset(player, GcItems.dragoniteHelmet, GcItems.dragoniteChestplate, GcItems.dragoniteLeggings, GcItems.dragoniteBoots))
+		{
+			this.effectPlayer(player, Potion.invisibility, 1);
+		}
+	}
+	
+	private boolean isWearingFullset(EntityPlayer player, Item helmet, Item chestplate, Item leggings, Item boots)
+	{
+		return player.inventory.armorItemInSlot(3) != null && player.inventory.armorItemInSlot(3).getItem() == helmet
+				&& player.inventory.armorItemInSlot(2) != null && player.inventory.armorItemInSlot(2).getItem() == chestplate
+				&& player.inventory.armorItemInSlot(1) != null && player.inventory.armorItemInSlot(1).getItem() == leggings
+				&& player.inventory.armorItemInSlot(0) != null && player.inventory.armorItemInSlot(0).getItem() == boots;
+	}
+	
+	private void effectPlayer(EntityPlayer player, Potion potion, int amplifier)
+	{
+		if (player.getActivePotionEffect(potion) == null || player.getActivePotionEffect(potion).getDuration() <= 1)
+			player.addPotionEffect(new PotionEffect(potion.id, 159, amplifier, true, true));
 	}
 
 }
